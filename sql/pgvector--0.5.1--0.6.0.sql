28ca878dc527dcd270cd676571b522d7c72494a8
reshke <reshke@double.cloud>
2025-05-06 13:14:02 +0300

    Fix pgvector corruption

--- sql/pgvector--0.5.1--0.6.0.sql
+++ sql/pgvector--0.5.1--0.6.0.sql
@@ -1,5 +1,69 @@
--- complain if script is sourced in psql, rather than via CREATE EXTENSION
-\echo Use "ALTER EXTENSION vector UPDATE TO '0.6.0'" to load this file. \quit
 
--- remove this single line for Postgres < 13
-ALTER TYPE vector SET (STORAGE = external);
+DO LANGUAGE PLPGSQL
+$$DECLARE r int;
+BEGIN
+        SELECT count(1) INTO r FROM pg_am WHERE amname = 'hnsw';
+        IF r = 0 THEN
+                RAISE NOTICE 'here';
+		-- remove this single line for Postgres < 13
+		ALTER TYPE vector SET (STORAGE = extended);
+
+		CREATE FUNCTION vector_accum(double precision[], vector) RETURNS double precision[]
+			AS 'MODULE_PATHNAME' LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
+
+		CREATE FUNCTION vector_avg(double precision[]) RETURNS vector
+			AS 'MODULE_PATHNAME' LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
+
+		CREATE FUNCTION vector_combine(double precision[], double precision[]) RETURNS double precision[]
+			AS 'MODULE_PATHNAME' LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
+
+		CREATE AGGREGATE avg(vector) (
+			SFUNC = vector_accum,
+			STYPE = double precision[],
+			FINALFUNC = vector_avg,
+			COMBINEFUNC = vector_combine,
+			INITCOND = '{0}',
+			PARALLEL = SAFE
+		);
+		CREATE FUNCTION l1_distance(vector, vector) RETURNS float8
+		AS 'MODULE_PATHNAME' LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
+
+		CREATE FUNCTION vector_mul(vector, vector) RETURNS vector
+			AS 'MODULE_PATHNAME' LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
+
+		CREATE OPERATOR * (
+			LEFTARG = vector, RIGHTARG = vector, PROCEDURE = vector_mul,
+			COMMUTATOR = *
+		);
+
+		CREATE AGGREGATE sum(vector) (
+			SFUNC = vector_add,
+			STYPE = vector,
+			COMBINEFUNC = vector_add,
+			PARALLEL = SAFE
+		);
+
+		CREATE FUNCTION hnswhandler(internal) RETURNS index_am_handler
+			AS 'MODULE_PATHNAME' LANGUAGE C;
+
+		CREATE ACCESS METHOD hnsw TYPE INDEX HANDLER hnswhandler;
+
+		COMMENT ON ACCESS METHOD hnsw IS 'hnsw index access method';
+
+		CREATE OPERATOR CLASS vector_l2_ops
+			FOR TYPE vector USING hnsw AS
+			OPERATOR 1 <-> (vector, vector) FOR ORDER BY float_ops,
+			FUNCTION 1 vector_l2_squared_distance(vector, vector);
+
+		CREATE OPERATOR CLASS vector_ip_ops
+			FOR TYPE vector USING hnsw AS
+			OPERATOR 1 <#> (vector, vector) FOR ORDER BY float_ops,
+			FUNCTION 1 vector_negative_inner_product(vector, vector);
+
+		CREATE OPERATOR CLASS vector_cosine_ops
+			FOR TYPE vector USING hnsw AS
+			OPERATOR 1 <=> (vector, vector) FOR ORDER BY float_ops,
+			FUNCTION 1 vector_negative_inner_product(vector, vector),
+			FUNCTION 2 vector_norm(vector);
+        END IF;
+END $$;
